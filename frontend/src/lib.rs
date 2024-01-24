713a9dda3f5cbd71639bcff1c02891358a8af70a
0xFloop <floopcrypto@yahoo.com>
2024-01-24 12:29:33 -0500

    spilt frontend from server, begin component structure

---
+++ frontend/src/lib.rs
@@ -0,0 +1,15 @@
+use leptos::*;
+
+#[component]
+pub fn UsernameInput() -> impl IntoView {
+    view! {<div>"this is the input component"</div>}
+}
+#[component]
+pub fn ChatWindow() -> impl IntoView {
+    view! {<div><p>"this is the chat window section"</p></div>}
+}
+
+#[component]
+pub fn NewMessageInput() -> impl IntoView {
+    view! {<div><p>"this is the chat window section"</p></div>}
+}
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,2 +1,18 @@
-pub fn main() {}
+use chat_frontend::*;
+use leptos::*;
+#[component]
+fn App() -> impl IntoView {
+    let _mesages = create_signal::<Vec<String>>(vec![]);
+    view! {
+        <div class="message-app">
 
+        <h1>"This is a basic chat app written entirely in rust"</h1>
+        <UsernameInput/>
+        <ChatWindow/>
+        <NewMessageInput/>
+        </div>
+    }
+}
+fn main() {
+    mount_to_body(App);
+}
